2d89bfeaab38ded5f10a84c5260a502b78250445
David Kostka <dkostka@eurofunk.com>
2019-12-12 20:35:18 +0100

    Initial commit

---
+++ src/main/java/com/kostka/efhomework/EfhomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.kostka.efhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfhomeworkApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(EfhomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kostka/efhomework/EfhomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kostka.efhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfhomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
