f1fa52609c0f91c18ec87d2f8df58790801035d1
David Kostka <dkostka@eurofunk.com>
2019-12-14 11:12:24 +0100

    add user assignment service + tests

---
+++ src/main/java/com/kostka/efhomework/service/management/assignment/UserAssignmentService.java
@@ -0,0 +1,11 @@
+package com.kostka.efhomework.service.management.assignment;
+
+/**
+ * TODO: think about generic solution for this service
+ * GroupAssignment + UserAssignment services are quite similar
+ */
+
+public interface UserAssignmentService {
+    void assignUserToGroup(String userName, String groupName);
+    void deAssignUserFromGroup(String userName, String groupName);
+}
